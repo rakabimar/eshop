89190a001b6e781f148b8b85f6d75f34b7db63b6
rakabimar <90183706+rakabimar@users.noreply.github.com>
2025-02-19 15:35:22 +0700

    [FIX]: Fix Homepage name to be resolved by thymeleaf

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
     @GetMapping("")
     public String mainPage() {
-        return "homePage";
+        return "Homepage";
     }
 }
