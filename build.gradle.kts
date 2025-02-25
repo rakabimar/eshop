61e9341e2357e1f5bea54ff140b37c7a2fe46154
rakabimar <rakabimaghaniendra22@gmail.com>
2025-02-25 09:36:22 +0700

    [FIX]: Fix analysis using Sonarqube (CSUI) instead of SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "eshop-rakabimar")
+        property("sonar.projectName", "eshop-rakabimar")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
